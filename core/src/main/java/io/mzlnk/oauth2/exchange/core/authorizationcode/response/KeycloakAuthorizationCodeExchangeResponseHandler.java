40bce57631abea57cfd85e91dcd686d46dd81359
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-06 18:44:47 +0100

    GH-10: provided java docs for response handlers

--- core/src/main/java/io/mzlnk/oauth2/exchange/core/authorizationcode/response/KeycloakAuthorizationCodeExchangeResponseHandler.java
+++ core/src/main/java/io/mzlnk/oauth2/exchange/core/authorizationcode/response/KeycloakAuthorizationCodeExchangeResponseHandler.java
@@ -2,24 +2,74 @@ package io.mzlnk.oauth2.exchange.core.authorizationcode.response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mzlnk.oauth2.exchange.core.ExchangeException;
-import io.mzlnk.oauth2.exchange.core.authorizationcode.response.dto.GoogleAuthorizationCodeExchangeResponse;
 import io.mzlnk.oauth2.exchange.core.authorizationcode.response.dto.KeycloakAuthorizationCodeExchangeResponse;
 import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
+/**
+ * Represents a handler of the incoming HTTP response during authorization code exchange for Keycloak auth provider.
+ * <p>
+ * The handler supports both successful and error incoming HTTP responses - based on the information posted on the official
+ * <a href="https://www.keycloak.org/docs/latest/authorization_services/">documentation site</a>
+ * </p>
+ * <br />
+ * The example successful response returned in JSON response body format:
+ * <pre>
+ * {
+ *   "access_token": "some-access-token",
+ *   "expires_in": 300,
+ *   "refresh_expires_in": 1800,
+ *   "refresh_token": "some-refresh-token",
+ *   "token_type": "bearer",
+ *   "not-before-policy": 0,
+ *   "session_state": "some-session-state",
+ *   "scope": "some-scope"
+ * }
+ * </pre>
+ * The example HTTP 4xx error response returned in JSON response body format:
+ * <pre>
+ * {
+ *   "error": "some-error"
+ * }
+ * </pre>
+ */
 public final class KeycloakAuthorizationCodeExchangeResponseHandler extends AbstractJsonBodyAuthorizationCodeExchangeResponseHandler<KeycloakAuthorizationCodeExchangeResponse> {
 
+    /**
+     * Constructs a handler with a given Jackson object mapper used during JSON body deserialization.
+     *
+     * @param objectMapper Jackson object mapper used to parse incoming JSON body
+     */
     public KeycloakAuthorizationCodeExchangeResponseHandler(@NotNull ObjectMapper objectMapper) {
         super(objectMapper);
     }
 
+    /**
+     * Construct an instance of a {@link KeycloakAuthorizationCodeExchangeResponse} response from given map of values.
+     *
+     * @param values non-null map of key-value pairs related to the exchange response
+     */
     @Override
     protected KeycloakAuthorizationCodeExchangeResponse convertValues(@NotNull Map<String, Object> values) {
         return KeycloakAuthorizationCodeExchangeResponse.from(values);
     }
 
+    /**
+     * Handles error HTTP response by throwing an {@link ExchangeException} which consists of an error message
+     * retrieved from the HTTP response.
+     *
+     * The exception message is retrieved from:
+     * <ul>
+     *     <li><code>error</code> field from JSON error response body if response is the HTTP 400 one</li>
+     *     <li>HTTP status message if response is different than the HTTP 400 one</li>
+     * </ul>
+     *
+     * @param response incoming error HTTP response
+     * @return none
+     * @throws ExchangeException exception which consists of an error message
+     */
     @Override
     protected KeycloakAuthorizationCodeExchangeResponse handleErrorResponse(@NotNull Response response) {
         return response.code() == 400
