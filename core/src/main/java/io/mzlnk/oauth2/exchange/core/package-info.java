0473892929d02eb1e9a4a79cc09fb652d5f01e82
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-06 16:16:19 +0100

    GH-10: provided package-info

---
+++ core/src/main/java/io/mzlnk/oauth2/exchange/core/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Classes related to core functionality for exchanging authorization code for a token in OAuth2 authorization code flow.
+ * <p>
+ * This package consists of ready-for-use exchanging clients related to most common authorization providers like Google,
+ * Facebook, Microsoft, etc. but also provides some configurable templates to implement exchanging functionality for
+ * custom providers with the least effort.
+ * </p>
+ *
+ */
+package io.mzlnk.oauth2.exchange.core;
---
+++ core/src/main/java/io/mzlnk/oauth2/exchange/core/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Utility classes used within the library.
+ */
+package io.mzlnk.oauth2.exchange.core.utils;
